27f707dbcd338910eca0d22f884e8e26ec334fea
Joash <guanzonjoashmatthew@gmail.com>
2024-04-22 01:04:29 +0800

    Added with open statement and odd and even analyzer statement

--- odd_even_extractor.py
+++ odd_even_extractor.py
@@ -1,8 +1,11 @@
+def main():
 # Read the numbers from the input file
-
+with open("numbers.txt", "r") as file:
+    numbers = [int(num) for num in file.read().split()]
 # Separate even and odd numbers
+even_numbers = [num for num in numbers if num % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
 
 # Write even numbers to even.txt
-
 
 # Write odd numbers to odd.txt
