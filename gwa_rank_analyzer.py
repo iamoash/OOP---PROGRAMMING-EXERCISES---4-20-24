21c8d6d2986ad45105892a57c63b5c94f99fcb32
Joash <guanzonjoashmatthew@gmail.com>
2024-04-22 16:28:54 +0800

    Added Pseudocode

---
+++ gwa_rank_analyzer.py
@@ -0,0 +1,11 @@
+# Open the File Of Student GWA
+
+# Create a file directory to store student names and GWAs
+
+# Analyze each line in the file
+
+# Find the student with the highest GWA
+
+# Output the student with the highest GWA
+
+    
