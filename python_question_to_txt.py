b18d511220b3627a9e495ab872d74981391eefbc
Joash <guanzonjoashmatthew@gmail.com>
2024-04-23 18:42:48 +0800

    Added Pseudocodes

---
+++ python_question_to_txt.py
@@ -0,0 +1,26 @@
+
+    # Open the file for writing ('w' mode)
+    
+    
+    # Loop indefinitely
+    
+        # Prompt the user to enter a line
+        
+        
+        # Append a newline character to the input line
+        
+        
+        # Write the line to the file
+        
+        
+        # Prompt the user to determine if there are more lines to enter
+        
+        
+        # If the user chooses 'n', exit the loop
+        
+    
+    # Close the file
+    
+
+# Call the writelines function to execute the code
+
