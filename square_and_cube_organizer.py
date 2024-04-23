6a75bdfb0e84a4aa346e3daec6ab82182b078d93
Joash <guanzonjoashmatthew@gmail.com>
2024-04-23 20:00:15 +0800

    Added Output Statements And Computational Statements

--- square_and_cube_organizer.py
+++ square_and_cube_organizer.py
@@ -9,11 +9,13 @@ def process_integers(source_file):
         # Iterate through the integer
         for num in integers:
             # Check if the number is even
-            
+            if num % 2 == 0:
                 # Calculate the square of even numbers and write to double.txt
-                
+                double_file.write(str(num ** 2) + '\n')
+            else:
             
                 # Calculate the cube of odd numbers and write to triple.txt
-                
+                triple_file.write(str(num ** 3) + '\n')
 
 # Call the method with the source file name
+process_integers('integers.txt')
