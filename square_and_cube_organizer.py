9fd06e9b63f6548dea5829dcc598cab5e42f565c
Joash <guanzonjoashmatthew@gmail.com>
2024-04-23 19:45:16 +0800

    Added Pseudocode

---
+++ square_and_cube_organizer.py
@@ -0,0 +1,20 @@
+
+    # Open the source file for reading
+    
+        # Read all integers from the source file
+        
+
+    # Open output files for writing
+    
+        # Iterate through the integer
+        
+            # Check if the number is even
+            
+                # Calculate the square of even numbers and write to double.txt
+                
+            
+                # Calculate the cube of odd numbers and write to triple.txt
+                
+
+# Call the method with the source file name
+
